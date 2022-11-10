63eff0ecb1740a68c09b05ea3ba32b1e3d200113
lay423 <junpang486@gmail.com>
2022-11-10 12:00:54 +0900

    [Update] findById()

--- src/main/java/com/mustache/bbs3/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs3/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs3.domain.entity.Article;
 import com.mustache.bbs3.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -24,6 +28,19 @@ public class ArticleController {
     public String create() {
         return "articles/new";
     }
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model){
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        log.info(optionalArticle.get().getTitle());
+        if(!optionalArticle.isEmpty()){
+            System.out.println("성공");
+            model.addAttribute("article", optionalArticle.get());
+            return "articles/show";
+        }else{
+            return "articles/error";
+        }
+    }
+
 
     @PostMapping("")
     public String articles(ArticleDto articleDto) {
